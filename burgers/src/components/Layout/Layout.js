cee81929c9f72c82ba78437e201795002b77c22f
Minwei Yao <dzrematt@gmail.com>
2019-09-17 23:57:29 +1000

    added toolbar and sidedrawer

--- burgers/src/components/Layout/Layout.js
+++ burgers/src/components/Layout/Layout.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 
-const layout = ( props ) => (
-    <Aux>
-        <div>Toolbar, SideDraw, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>            
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    
+    
+    sideDrawClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+    
+    sideDrawOpenHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer}})
+    }
+    
+    render() {
+        return (    
+            <Aux>
+            <Toolbar open={this.sideDrawOpenHandler}/>
+            <SideDrawer 
+            closed={this.sideDrawClosedHandler}
+            open={this.state.showSideDrawer}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>     
+        )
+    }  
+}
 
-export default layout;
+
+export default Layout;
---
+++ burgers/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+import classes from './Toolbar.css';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.open}/>
+        <Logo height="80%"/>
+        <nav className={classes.DesktopOnly}>
+          <NavigationItems />
+        </nav>
+    </header>
+)
+
+export default toolbar;
