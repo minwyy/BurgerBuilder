50dacb1b29a710d0700f7faac187915acff19872
Minwei Yao <dzrematt@gmail.com>
2020-05-02 21:45:38 +1000

    created customized input form and set onChangeHandler for user to fill the form

--- burgers/src/containers/Checkout/ContactData/ContactData.js
+++ burgers/src/containers/Checkout/ContactData/ContactData.js
@@ -3,35 +3,81 @@ import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css';
+
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postcode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZhaZha J'
+                },
+                value: ''
+            },
+  
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Wattle St'
+                },
+                value: ''
+            },
+            postcode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: '2009'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Australia'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'zzhaj@uts.edu.au'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}]
+                },
+                value: ''
+            },
+            paymentMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{value: 'returnservices', displayValue: 'Returnservices'},
+                    {value: 'new customer', displayValue: 'New customer'}]
+                },
+                value: ''
+            }
         },
         loading: false
     }
+
     orderHandler = ( e ) => {
         // e.preventDefault();
         this.setState({loading: true})
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Zhazha J',
-                address: {
-                    street: 'Wattle st',
-                    postcode: '2009',
-                    country: 'Australia'
-                },
-                email: 'zzhaj@uts.edu.au',
-            },
-            deliveryMethod: 'car',
-            paymentMethod: 'returnService'
+ 
         }
         axios.post('/orders.json', order)
         .then (response => {
@@ -44,15 +90,32 @@ class ContactData extends Component {
         // console.log('hahahahhhhhhhhhhhh');
     }
 
+    inputChangeHandler = (event, inputIdentifier) => {
+        const updatedForm = {...this.state.orderForm};
+        const updatedFormElement = {...updatedForm[inputIdentifier]};
+        updatedFormElement.value = event.target.value;
+        updatedForm[inputIdentifier]=updatedFormElement;
+        this.setState({orderForm: updatedForm});
+    }
+
     render () {
         // console.log(this.props.price);
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({id:key, config: this.state.orderForm[key]})
+        }
+        
         let form = (            
         <React.Fragment>
             <form>
-                <input className={classes.Input} type="Text" name="name" placeholder="Your name"/>
-                <input className={classes.Input} type="email" name="email" placeholder="Your email"/>
-                <input className={classes.Input} type="Text" name="street" placeholder="Street"/>
-                <input className={classes.Input} type="Text" name="postal" placeholder="Postal code"/>
+                {formElementsArray.map(forElement => (
+                    <Input 
+                        key={forElement.id}
+                        elementType={forElement.config.elementType}
+                        elementConfig={forElement.config.elementConfig}
+                        value={forElement.config.value}
+                        change={(event) => (this.inputChangeHandler(event, forElement.id))} />
+                ))}
             </form>
             <Button btnType='Success' clicked={this.orderHandler}>Order</Button>
         </React.Fragment>
