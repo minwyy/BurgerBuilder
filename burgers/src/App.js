4fb77b3dd876ee2a211f1770cea54595e7fb88c9
Minwei Yao <dzrematt@gmail.com>
2020-05-23 00:53:03 +1000

    routes guarded locally at frontend

--- burgers/src/App.js
+++ burgers/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './components/Layout/Layout';
@@ -16,21 +16,38 @@ class App extends Component {
   componentDidMount () {
     this.props.onAuthCheckLocal();
   }
-
+  
   render() {
+    let routes = (   
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+    </Switch>);
+
+    if (this.props.isAuthenticated) {
+      routes = (
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -38,4 +55,4 @@ const mapDispatchToProps = dispatch => {
   return {onAuthCheckLocal: () => dispatch(actions.authCheckLocal())}
 }
 
-export default connect(null, mapDispatchToProps) (App);
+export default connect(mapStateToProps, mapDispatchToProps) (App);
